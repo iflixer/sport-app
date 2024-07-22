018abc5ee1941ebadcaef70a492c7d6fa9f85805
andyUA <grenway.andy@gmail.com>
2024-07-22 16:22:19 +0300

    Second menu process

--- js/app.js
+++ js/app.js
@@ -34,3 +34,67 @@ document.addEventListener('change', function(event) {
         }
     }
 });
+
+$(document).ready(function() {
+    // Show/hide radio group on button click
+    $('.main-bet-but').on('click', function() {
+        $('.slidefrombot').toggleClass('show');
+        $('body').toggleClass('hasover');
+    });
+
+    // Hide radio group on clicking outside or swiping down
+    $(document).on('click touchstart', function(e) {
+        if (!$(e.target).closest('.slidefrombot, .main-bet-but').length) {
+            $('.slidefrombot').removeClass('show');
+            $('body').removeClass('hasover');
+        }
+    });
+
+    // Prevent hiding if clicking inside the radio group container
+    $('.slidefrombot').on('click touchstart', function(e) {
+        e.stopPropagation(); // Prevent event from bubbling up
+    });
+
+    // Swipe down gesture to close radio group
+    var startY = 0;
+    $(document).on('touchstart', function(e) {
+        startY = e.originalEvent.touches[0].pageY;
+    });
+
+    $(document).on('touchmove', function(e) {
+        var touchY = e.originalEvent.touches[0].pageY;
+        if (touchY > startY && (touchY - startY) > 50) { // Swipe down threshold
+            $('.slidefrombot').removeClass('show');
+            $('body').removeClass('hasover');
+        }
+    });
+
+    $('#betters input[name="option"]').on('change', function() {
+        var radioValue = $('#betters input[name="option"]:checked').val();
+        $('.main-bet-but').text(radioValue);
+    });
+
+    $(document).on('click','.upcomebut', function() {
+       $('.upcomebut').removeClass('active');
+       if($(this).hasClass('nextfull')) {
+           $('.upcomebut').not(('.upcomebut.full')).hide();
+           $(this).next('.upcomebut.full').addClass('active');
+           $('.closeall').addClass('show').show();
+       }else{
+           $(this).addClass('active');
+       }
+    });
+
+    $(document).on('click','.upcomein', function() {
+        $('.upcomein:not(.parent)').removeClass('active');
+            $(this).addClass('active');
+    });
+    $(document).on('click','.closeall', function() {
+
+        $(this).removeClass('show').hide();
+        $('.upcomebut.full').removeClass('active').hide();
+        $('#upcome>.upcomebut:not(.full)').show();
+        $('.upcomebut:first-child').addClass('active');
+    });
+
+});
